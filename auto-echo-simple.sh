3ca4b6dfa1f9877468159cf726f575ff9d6ee831
J3rry <100356532+jerrygems@users.noreply.github.com>
2023-01-20 09:01:58 +0530

    an update
    players may use chmod or chattr to prevent writing for other players so this script can be more helpfull in that case

--- auto-echo-simple.sh
+++ auto-echo-simple.sh
@@ -1,1 +1,1 @@
-while [[ $(cat /root/king.txt) != "[usernameHere]" ]]; do echo "[usernamehere]" >> /root/king.txt; done
+while [[ $(cat /root/king.txt) != "[usernameHere]" ]]; do chmod chmod 755 /root/king.txt; chattr -i /root/king.txt; echo "[usernamehere]" >> /root/king.txt; done
