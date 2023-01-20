98998501309731e6e9b7a68b0fa435fbf4a74519
J3rry <100356532+jerrygems@users.noreply.github.com>
2023-01-20 09:37:03 +0530

    Update auto-echo.sh

--- auto-echo.sh
+++ auto-echo.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 while :
 do
+        eval "chmod 755 /root/king.txt"
+        eval "chattr -i /root/king.txt"
         eval "echo [usernameHere] >> /root/king.txt"
         eval " > /root/king.txt"
 done
